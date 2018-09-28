7ab86554171f180f1e729fcc9f0f0c41b188f515
Nanoteck137 <patrik.millvik@gmail.com>
2018-09-29 01:27:59 +0200

    Regenerated the project

--- core/src/net/nanoteck137/game/Program.java
+++ core/src/net/nanoteck137/game/Program.java
@@ -5,13 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector3;
 
 public class Program extends ApplicationAdapter {
 
